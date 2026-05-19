63f7830f06fcb6c4d90630d063194b76d4baaac5
JimmyGAR <jimmy.garnier1110@gmail.com>
2026-05-19 08:20:07 +0200

    Ajout des entités Bug et User (partie 3)

--- src/Bug.php
+++ src/Bug.php
@@ -25,6 +25,9 @@ class Bug
 
     private $products;
 
+    private User $engineer;
+    private User $reporter;
+
     public function __construct()
     {
         $this->products = new ArrayCollection();
@@ -57,5 +60,31 @@ class Bug
     public function getStatus(): string
     {
         return $this->status;
+    }
+    public function setEngineer(User $engineer): void
+    {
+        $engineer->assignedToBug($this);
+        $this->engineer = $engineer;
+    }
+    public function setReporter(User $reporter): void
+    {
+        $reporter->addReportedBug($this);
+        $this->reporter = $reporter;
+    }
+    public function getEngineer(): User
+    {
+        return $this->engineer;
+    }
+    public function getReporter(): User
+    {
+        return $this->reporter;
+    }
+    public function assignToProduct(Product $product): void
+    {
+        $this->products[] = $product;
+    }
+    public function getProducts()
+    {
+        return $this->products;
     }
 }
